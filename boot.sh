27b91c20350c8e86dae9d03547842687822c6cf5
Vigneshthanga <vigneshkumar.thangarajan@gmail.com>
2019-11-04 16:20:54 -0800

    including common package

--- boot.sh
+++ boot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -e
 
 # start service discovery task in the background
-#if [ "$SERVICE_URL" != "/login" ]; then
-#    python -c "from lockoncrm_common.container import register; register()" &
-#fi
+if [ "$SERVICE_URL" != "/index" ]; then
+    python -c "from lockoncrm_common.container import register; register()" &
+fi
 
 # run web server
 exec gunicorn -b 0.0.0.0:5000 app:app
